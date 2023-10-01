8e3f120e7b73a89435e9f203ad04bcb309fda693
Kulonbekov <kulonbekov@gmail.com>
2023-10-01 21:47:25 +0600

    add project

---
+++ navigation/src/main/java/com/axelor/apps/navigation/integration/org/tempuri/RequestTD.java
@@ -0,0 +1,62 @@
+
+package org.tempuri;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="request" type="{http://tempuri.org/}TDInfoRequest" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "request"
+})
+@XmlRootElement(name = "RequestTD")
+public class RequestTD {
+
+    protected TDInfoRequest request;
+
+    /**
+     * Gets the value of the request property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link TDInfoRequest }
+     *     
+     */
+    public TDInfoRequest getRequest() {
+        return request;
+    }
+
+    /**
+     * Sets the value of the request property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link TDInfoRequest }
+     *     
+     */
+    public void setRequest(TDInfoRequest value) {
+        this.request = value;
+    }
+
+}
